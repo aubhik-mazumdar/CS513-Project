32f1392024a012fd3eebd56bc9fe91f4536a3f80
aubhik-mazumdar <aubhik.mazumdar@gmail.com>
2018-12-02 16:27:34 -0500

    [DONE] Finished Coding

--- cars_kmeans.R
+++ cars_kmeans.R
@@ -1,13 +1,14 @@
+rm(list = ls())
 setwd('~/Desktop/Fall_2018/KDD/CS513-Project/datasets')
 data = read.csv('data.csv',na.strings = 'N/A')
 
 # Remove the column for Vehicle.Size
 data <- data[,-10]
 
-which(is.na(data))
 data <- na.omit(data)
 data$Make <- factor(data$Make)
 data$Make
+
 set.seed(100)
 idx <- sample(c(1:nrow(data)),0.75*nrow(data))
 train <- data[idx,]
@@ -15,21 +16,28 @@ test <- data[-idx,]
 
 #average_price_by_fuel_type
 library(caroline)
-average_price_by_fuel_type <- groupBy(data,by='Engine.Fuel.Type',clmns='MSRP',aggregation = 'mean',na.rm = TRUE,full.names = TRUE)
-l <- data.frame(levels(data$Engine.Fuel.Type))
-t <- data.frame(l,average_price_by_fuel_type)
-levels(data$Engine.Fuel.Type)
-?colnames
-colnames(t) <- c("Engine type","Average Price")
+# Calculating the average price for each car type
+average_price_by_fuel_type <- groupBy(data,by='Engine.Fuel.Type',clmns='MSRP',
+                                      aggregation = 'mean',na.rm = TRUE,full.names = TRUE)
+
+# Create the table for average price
+average_price_engine <- data.frame(Engine.Type = levels(data$Engine.Fuel.Type),
+                                   average_price=average_price_by_fuel_type[,1])
+
+# Calculating the average price for each car type
+average_price_by_make <- groupBy(data,by='Make',clmns='MSRP',
+                                      aggregation = 'mean',na.rm = TRUE,full.names = TRUE)
+
+# Create the table for average price
+average_price_make <- data.frame(Make = levels(data$Make),
+                                   average_price=average_price_by_make[,1])
+
+
 clusters <- kmeans(data[,c(5,6,9,12,13,14,15)],47,iter.max = 100)
 data$MakeCluster <- as.factor(clusters$cluster)
 str(clusters)
 
-clusters <- kmeans(data[,c()],47,iter.max = 100)
-data$MakeCluster <- as.factor(clusters$cluster)
-str(clusters)
-
-
 # Average price for each cluster
 library(caroline)
 g <- groupBy(data,by='MakeCluster',aggregation = c('mean'),clmns='MSRP',na.rm=TRUE, full.names = TRUE)
+g
--- cars_knn.R
+++ cars_knn.R
@@ -1,21 +1,23 @@
+rm(list = ls())
 setwd('~/Desktop/Fall_2018/KDD/CS513-Project/datasets')
 data = read.csv('data.csv',na.strings = 'N/A')
 
-# Remove the column for Vehicle.Size
-data <- data[,-10]
+# Remove the column for Market.Category, Vehicle.Size
+data <- data[,c(-10,-11)]
 
-which(is.na(data))
+
 data <- na.omit(data)
 data$Make <- factor(data$Make)
-data$Make
+
 set.seed(100)
 idx <- sample(c(1:nrow(data)),0.75*nrow(data))
 train <- data[idx,]
 test <- data[-idx,]
+
 library(kknn)
-model <- kknn(Make ~.,train,test,k=7)
+model <- kknn(Make ~.,train,test,k=5)
 fit <- fitted(model)
-#ref <- predict(model,test)
+
 t <- table(actual = test$Make,fit)
 View(t)
 
